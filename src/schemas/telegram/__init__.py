bb9bc43d34949f3fd5a7d4426093912513f92a8c
Sourangshu Pal <paul.visionai@gmail.com>
2026-05-28 10:07:03 +0530

    fix(telegram): remove imports of non-existent schema modules

--- src/schemas/telegram/__init__.py
+++ src/schemas/telegram/__init__.py
@@ -1,11 +1,1 @@
-from .commands import TelegramCommand
-from .messages import TelegramMessageRequest, TelegramMessageResponse, TelegramUpdate
-from .user_settings import UserSettings
-
-__all__ = [
-    "TelegramCommand",
-    "TelegramMessageRequest",
-    "TelegramMessageResponse",
-    "TelegramUpdate",
-    "UserSettings",
-]
+__all__: list[str] = []
